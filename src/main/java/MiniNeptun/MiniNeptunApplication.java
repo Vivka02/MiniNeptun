800db876a036409b5ae3361e3d0efd89c5d4fe81
Vivka <vivien.koosz@gmail.com>
2020-04-13 14:18:50 +0200

    Initial commit

---
+++ src/main/java/MiniNeptun/MiniNeptunApplication.java
@@ -0,0 +1,13 @@
+package MiniNeptun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniNeptunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniNeptunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MiniNeptun/MiniNeptunApplicationTests.java
@@ -0,0 +1,13 @@
+package MiniNeptun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniNeptunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
